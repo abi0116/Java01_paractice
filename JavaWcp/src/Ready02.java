53bacb985e88860b4034e4eb544ab1e6826e4dc2
abi0116 <kosukeabiru.www@gmail.com>
2021-05-04 22:05:34 +0900

    Java4章

--- JavaWcp/src/Ready02.java
+++ JavaWcp/src/Ready02.java
@@ -3,5 +3,12 @@ public class Ready02 {
   public static void main(String[] args) {
       System.out.println("Hello World!");
       System.out.println("Hello Java!");
+      String str1;  // String型の変数str1を宣言
+      str1 = "阿比留";  // 変数str1を初期化
+      System.out.println(str1);  // 変数str1を出力
+      str1 = "温故知新"; // 変数str1に温故知新を再代入
+      System.out.println(str1); // 変数str1を出力
+      final int MY_BIRTHDAY = 116 ; // finalを用いてint型の定数MY_BIRTHDAYを宣言
+      System.out.println(MY_BIRTHDAY); // 定数MY_BIRTHDAYを出力
   }
 }
